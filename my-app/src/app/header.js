64c9feab2b7a4faba739c8bf253fa7bb4e36d4ce
Rafael <Rafael.mattos468@gmail.com>
2024-06-26 20:14:38 -0300

    feat: criação do projeto em nextJS

---
+++ my-app/src/app/header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <li>home</li>
+            <li>sobre</li>
+            <li>localização</li>
+            <li>contatos</li>
+        </header>
+    )
+}
