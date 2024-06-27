7d0a2a7c5209972f52d7fb5f297edceca80b1e2f
Rafael <Rafael.mattos468@gmail.com>
2024-06-26 21:57:24 -0300

    update

--- my-app/src/app/main.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function main() {
-    return (
-       <main>main</main>
-    )
-}
---
+++ my-app/src/app/page.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+export default function Home() {
+  return (
+    <div className="h-full"> 
+      <header className=" flex flex-col items-center justify-between">
+        header
+      </header>
+      <main className="flex h-screen flex-col items-center justify-between p-24 bg-slate-950">
+      <p className="text-green-500">Main</p>
+
+      </main>
+  
+    </div>
+
+  );
+}
