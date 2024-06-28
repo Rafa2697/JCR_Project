0d7595f16e10a5d8a7f21cd7b4fca86ad4c66612
Rafael <Rafael.mattos468@gmail.com>
2024-06-28 20:44:12 -0300

    feat: importando e inserindo os componentes da seçoes.

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,17 +1,19 @@
 
-import Logo from '@/app/components/logo'
+import Logo from '@/app/components/sections/logo'
 import Sobre from '@/app/components/sections/sobre'
-
+import Services from '@/app/components/sections/services'
+import Header from "@/app/components/header"
 
 export default function Home() {
   return (
-    <div className='h-full bg-neutral-900' >
+    <div className='' >
 
-      
-        <Logo/>
-        <Sobre/>
+      <Header/>
+      <Logo />
+      <Sobre />
+      <Services/>
 
-   
+
 
     </div>
 
