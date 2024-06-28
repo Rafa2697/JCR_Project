2bcf9475f1a762137972bf2671a556db2a5ad01f
Rafael <Rafael.mattos468@gmail.com>
2024-06-28 20:48:25 -0300

    feat: criando componente de seviços

---
+++ my-app/src/app/components/sections/services.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Services() {
+
+    return (
+        <div className="flex flex-col items-center h-auto justify-center  bg-neutral-900">
+            <h1 className="text-emerald-50 text-3xl ">Serviços</h1>
+            <section className="h-screen text-emerald-50 flex items-center">
+                <h1>Portões em Aluminio</h1>
+            </section>
+            <section className="h-screen text-emerald-50 flex items-center" >
+                <h1>Móveis planejados em Alumino</h1>
+            </section>
+            <section className="h-screen text-emerald-50 flex items-center">
+                <h1>Vidro temperado</h1>
+            </section>
+        </div>
+    )
+
+}
