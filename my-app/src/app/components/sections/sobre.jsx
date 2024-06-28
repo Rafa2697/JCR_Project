35481e2f2d59ee3e6be460cc1ff42318ead98c97
Rafael <Rafael.mattos468@gmail.com>
2024-06-28 20:46:34 -0300

    styles: ajustes no estilo

--- my-app/src/app/components/sections/sobre.jsx
+++ my-app/src/app/components/sections/sobre.jsx
@@ -3,9 +3,9 @@
 export default function Sobre() {
 
     return (
-        <div className="flex flex-col items-center mt-10 ">
-            <h1>Sobre</h1>
-            <p className="text-green-500">Em 2017, Cesar Augusto Ribeiro fundou a "JCR - Esquadrias e Vidros Temperados", com o objetivo de oferecer à região soluções completas em esquadrias e vidros temperados, aliando qualidade superior com um atendimento personalizado.
+        <div className="flex flex-col items-center h-screen justify-center">
+            <h1 className="text-green-50 text-3xl space-y-3">Sobre</h1>
+            <p className="text-green-50 w-3/6 text-justify">Em 2017, Cesar Augusto Ribeiro fundou a "JCR - Esquadrias e Vidros Temperados", com o objetivo de oferecer à região soluções completas em esquadrias e vidros temperados, aliando qualidade superior com um atendimento personalizado.
 
                 Ao longo de sua trajetória, a JCR se consolidou como referência em confiança e profissionalismo, atendendo às mais diversas necessidades de seus clientes, desde obras residenciais até grandes projetos comerciais.</p>
         </div>
