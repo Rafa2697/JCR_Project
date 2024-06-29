0183e79f1fc5e87114fcc3801260e9cbc9ebde7e
Rafael <Rafael.mattos468@gmail.com>
2024-06-29 19:53:12 -0300

    feat: inserindo o componente de contato

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -2,15 +2,17 @@
 import Logo from '@/app/components/sections/logo'
 import Sobre from '@/app/components/sections/sobre'
 import Services from '@/app/components/sections/services'
+import Contact from '@/app/components/sections/contact'
 import Header from "@/app/components/header"
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Header/>
       <Logo />
       <Sobre />
       <Services/>
+      <Contact/>
     </div>
 
   );
