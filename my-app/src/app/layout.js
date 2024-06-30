85032acdd4894106202385bca6911e2e341e7a7c
Rafael <Rafael.mattos468@gmail.com>
2024-06-30 19:04:44 -0300

    feat: update favicon

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "JCR Esquadrias",
+  title: "Esquadrias Aluminio",
   description: "Esquadria e Aluminios",
 };
 
