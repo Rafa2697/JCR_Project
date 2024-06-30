95ae3b6359510364402ed466533a7382650f93b6
Rafael <Rafael.mattos468@gmail.com>
2024-06-30 16:59:38 -0300

    styles: adicionando footer

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -4,6 +4,7 @@ import Sobre from '@/app/components/sections/sobre'
 import Services from '@/app/components/sections/services'
 import Contact from '@/app/components/sections/contact'
 import Header from "@/app/components/header"
+import Footer from './components/footer'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Sobre />
       <Services/>
       <Contact/>
+      <Footer/>
     </div>
 
   );
