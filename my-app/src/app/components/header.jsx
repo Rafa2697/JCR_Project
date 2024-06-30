f34de378361671bdf8eccfda25a56aac14c9ed5d
Rafael <Rafael.mattos468@gmail.com>
2024-06-30 12:44:44 -0300

    feat: criando botão menu

--- my-app/src/app/components/header.jsx
+++ my-app/src/app/components/header.jsx
@@ -1,9 +1,20 @@
+'use client'
 
+import { useState} from "react";
+import { GrMenu } from "react-icons/gr";
 
 export default function Header(){
+
+    const [menu, setMenu] = useState(false)
+    const toggleMenu = () => {
+        setMenu(!menu); // Update menu state to its opposite value
+        console.log(menu)
+      };
+     
     return(
-        <nav className="fixed flex justify-center w-full fade-in z-40">
-            <ul className="flex  gap-6 justify-center text-zinc-50 m-7 ">
+        <nav className="fixed flex justify-start md:justify-center w-full fade-in z-40">
+            <GrMenu className="text-green-50 m-4 size-8 cursor-pointer" onClick={toggleMenu}/>
+            <ul className={` gap-6 justify-center text-zinc-50 m-7 ${menu ? 'flex' : 'hidden'}`} >
                 <li className="cursor-pointer"><a href="#about">Sobre</a></li>
                 <li className="cursor-pointer"><a href="#service">Serviços</a></li>
                 <li className="cursor-pointer"><a href="#contact">Contato</a></li>
--- my-app/src/app/components/sections/contact.jsx
+++ my-app/src/app/components/sections/contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
             <h1 className="text-green-50 text-6xl mt-7">Contato</h1>
             <div className='flex flex-col md:flex-row w-full md:w-3/4'>
                 <div className='flex justify-center w-full'>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3639.373513754313!2d-46.80674109999999!3d-24.193694699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d1d5426cc8347d%3A0xd989fb76b680ea44!2sR.%20Peru%C3%ADbe%2C%20312%20-%20Praia%20dos%20Sonhos%2C%20Itanha%C3%A9m%20-%20SP%2C%2011740-000!5e0!3m2!1spt-BR!2sbr!4v1719689474479!5m2!1spt-BR!2sbr" className='w-full  md:h-full m-3' referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3639.373513754313!2d-46.80674109999999!3d-24.193694699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d1d5426cc8347d%3A0xd989fb76b680ea44!2sR.%20Peru%C3%ADbe%2C%20312%20-%20Praia%20dos%20Sonhos%2C%20Itanha%C3%A9m%20-%20SP%2C%2011740-000!5e0!3m2!1spt-BR!2sbr!4v1719689474479!5m2!1spt-BR!2sbr" className='w-full  md:h-full m-3'></iframe>
                 </div>
                 <div className='flex justify-center w-full h-1/2' >
                     <a rel="stylesheet" href="https://wa.me/5513997483229?text=Olá,%20gostaria%20de%20fazer%20um%20orçamento!" target='_blanck' className='flex justify-center items-center w-full gap-4'>
